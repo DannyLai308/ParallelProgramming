2d690c57431bcf2297f64c9094fd09687855554c
DannyLai308 <56664017+DannyLai308@users.noreply.github.com>
2024-06-24 19:06:42 -0400

    changed approach to using semaphore

--- dining_philo.py
+++ dining_philo.py
@@ -1,124 +1,102 @@
-import sys
-import random
 import threading
+import random
 import time
-
-class Fork:
-    def __init__(self):
-        self.is_Used = False
-
-    def take_fork(self):
-        if not self.is_Used:
-            self.is_Used = True
-            return True
-        return False
-        
-    def put_fork_down(self):
-        self.is_Used = False
-
+import sys
 
 class Philosopher(threading.Thread):
-    def __init__(self, philo_id, left_fork, right_fork):
-        super().__init__()
-        self.philo_id = philo_id
-        self.left_fork = left_fork
-        self.right_fork = right_fork
-        self.random = random.Random()
-        self.is_deadlock = False  
-        self.has_eaten = False
-        self.meals = 0
-       
+  def __init__(self, philo_id, forks, terminate_event):
+    super().__init__()
+    self.philo_id = philo_id + 1 
+    self.left_fork = forks[philo_id]
+    self.right_fork = forks[(philo_id + 1) % len(forks)]
+    self.terminate_event = terminate_event
+    self.random = random.Random()
+    self.meals = 0
 
-    # Main function to simulate the dining philosophers problem. Contains the main loop
-    # where philosophers keeps altering between thinking and eating
-    def run(self):
-        while not self.is_deadlock:
-            self.philo_think() # Starts the simulation with the philosopher thinking
 
-            print(f"Philosopher # {self.philo_id} wants to eat")
-            if self.left_fork.take_fork():
-                # Taking left fork for a random length of time
-                take_left_fork = self.random.randint(1, 100)
-                time.sleep(take_left_fork/1000)
-                print(f"Philosopher # {self.philo_id} taking left fork")
 
-                # Taking right fork for a random length of time
-                if self.right_fork.take_fork():
-                    print(f"Philosopher # {self.philo_id} taking right fork")
-                    take_right_fork = self.random.randint(1, 100)
-                    time.sleep(take_right_fork/1000)
-                    self.philo_eat()
-                    self.meals += 1
+  def run(self):
+    while not self.terminate_event.is_set():
+      # Thinking process
+      self.philo_think()
 
-                    # Putting down one fork at a time after eating, at a random length of time
-                    putDown_left_fork = self.random.randint(1, 100)
-                    time.sleep(putDown_left_fork/1000)
-                    self.left_fork.put_fork_down()
-                    print(f"Philosopher # {self.philo_id} putting down left fork")
+      # Transitioning to taking forks to eat
+      print(f"Philosopher # {self.philo_id} wants to eat")
+   
+      # Picking up left fork
+      if not self.left_fork.acquire(timeout=0.6):  # Acquire left fork with timeout to wait for potential release of fork from neighbor philosopher
+        print(f"Philosopher # {self.philo_id} couldn't pick up left fork, get back to thinking")
+        continue  # Skip if can't get left fork within timeout
 
-                    putDown_right_fork = self.random.randint(1, 100)
-                    time.sleep(putDown_right_fork/1000)
-                    self.right_fork.put_fork_down()
-                    print(f"Philosopher # {self.philo_id} putting down right fork")
-                
-                else:
-                    print(f"Philosopher # {self.philo_id} couldn't take right fork")
-                    self.has_eaten = False
-                    # Put down left fork after failing to take right fork
-                    self.left_fork.put_fork_down()
-                    print(f"Philosopher # {self.philo_id} putting down left fork ")
-                    
-            else:
-                self.has_eaten = False
-                print(f"Philosopher # {self.philo_id} couldn't take left fork")
+      time.sleep(random.randint(1, 100)/1000) # Random length of time to pick up left fork
+      print(f"Philosopher # {self.philo_id} picked up left fork")
 
-            # Detect deadlock 
-            if  not self.is_deadlock and not self.left_fork.is_Used and not self.right_fork.is_Used and not self.has_eaten:
-                self.is_deadlock = True
-                print(f"Philosopher # {self.philo_id} reached deadlock")
+      if not self.right_fork.acquire(timeout=0.6):  # Acquire right fork with timeout to wait for potential release of fork from neighbor philosopher
+        print(f"Philosopher # {self.philo_id} couldn't pick up right fork within timeout")
+        #self.left_fork.release()  # Put down left fork if right fork is not available
+        break  # Break out of loop if can't get right fork within timeout, since it means the neighbor philosopher is holding to the right fork
 
+      time.sleep(random.randint(1, 100)/1000) # Random length of time to pick up right fork
+      print(f"Philosopher # {self.philo_id} picked up right fork")
+
+      # Eating process
+      self.philo_eat()
+      self.meals += 1
+
+      # Putting down both forks after eating
+      self.left_fork.release()
+      time.sleep(random.randint(1, 100)/1000) # Random length of time to put down left fork
+      print(f"Philosopher # {self.philo_id} put down left fork")
+
+      self.right_fork.release()
+      time.sleep(random.randint(1, 100)/1000) # Random length of time to put down right fork
+      print(f"Philosopher # {self.philo_id} put down right fork")
+
+    # Detect potential deadlock for current philosopher
+    print(f"Philosopher # {self.philo_id} potentially reached deadlock.")
+
+  def philo_think(self):
+    # The philosopher thinks for a random length of time
+      think_time = self.random.randint(1, 100)
+      print(f"Philosopher # {self.philo_id} is thinking for {think_time}ms")
+      time.sleep(think_time/1000)
+
+  def philo_eat(self):
+    # The philosopher eats for a random length of time
+      eat_time = self.random.randint(1, 100)
+      print(f"Philosopher # {self.philo_id} is eating for {eat_time}ms")
+      time.sleep(eat_time/1000)
     
-    def philo_think(self):
-        # The philosopher thinks for a random length of time
-        think_time = self.random.randint(1, 100)
-        print(f"Philosopher # {self.philo_id} is thinking for {think_time}ms")
-        time.sleep(think_time/1000)
 
-    def philo_eat(self):
-        # The philosopher eats for a random length of time
-        self.has_eaten = True
-        eat_time = self.random.randint(1, 100)
-        print(f"Philosopher # {self.philo_id} is eating for {eat_time}ms")
-        time.sleep(eat_time/1000)
-
-    
 def dining_simulation(number_Of_philo):
-    forks = [Fork() for _ in range(number_Of_philo)]
-    philosophers = [Philosopher(index + 1, forks[index], forks[(index + 1) % number_Of_philo]) for index in range(number_Of_philo)]
+  forks = [threading.Semaphore(1) for _ in range(number_Of_philo)]
+  terminate_event = threading.Event()
+  philosophers = [Philosopher(i, forks.copy(), terminate_event) for i in range(number_Of_philo)]
 
-    # Starts all threads for philosophers and waits for them to finish
-    for philo in philosophers:
-        philo.start()
-    for philo in philosophers:
-        philo.join()
+  # Start all philosopher threads
+  for philo in philosophers:
+    philo.start()
+  for philo in philosophers:
+    philo.join()
 
-    print("Deadlock occured for all philosophers")
+  print("Deadlock found! All philosophers potentially reached dead lock.")
+  print("\n")
+  print("# of philosophers    | Percentage of meals               | Total number of meals before deadlock")
+  total_meals = 0
+  for philo in philosophers:
+    total_meals += philo.meals
 
-    print("# of philosophers    | Percentage of meals               | Total number of meals before deadlock")
-    total_meals = 0
-    for philo in philosophers:
-        total_meals += philo.meals
-    print()
-    print(f"{number_Of_philo}                   | ", end=(" "))
-    for philo in philosophers:    
-        meals_percentage = (philo.meals / total_meals) * 100 if total_meals > 0 else 0
-        print(f"{meals_percentage:.1f}", end=", ")
-    print(f"                        | {total_meals}")
+  print(f"{number_Of_philo}                    | ", end=(" "))
+  for philo in philosophers:    
+    meals_percentage = (philo.meals / total_meals) * 100 if total_meals > 0 else 0
+    print(f"{meals_percentage:.1f}", end=", ")
+  print(f"                       | {total_meals}")
+
+  terminate_event.set()
 
 if __name__ == "__main__":
-    # if len(sys.argv) < 2:
-    #     print("Error: Please specify the number of philosophers (at least 2)")
-    #     exit(1)
-
-    number_Of_philo = 3
+    if len(sys.argv) < 2:
+        print("Error: Please specify the number of philosophers (at least 2)")
+        exit(1)
+    number_Of_philo = int(sys.argv[1])
     dining_simulation(number_Of_philo)
